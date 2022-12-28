77c4cc58f9b8123a79c00e021c71e9a73bdc6ee8
Kais Al Balkhi <qaiscyberlife@gmail.com>
2022-12-28 11:33:00 -0500

    solved up to 05-Ins activity

---
+++ in-class-workspace/03-Module/03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -0,0 +1,28 @@
+// Write Your JavaScript Code Here
+
+// Declares Variables
+var personName;
+var pets;
+var funFact;
+
+// Assigns values to variables
+personName = "Qais";
+pets = "1";
+funFact = "My kitten eats 7 cans a day";
+
+// Logs the variable in the console
+
+console.log('My name is ' + personName + '.')
+console.log('I have ' + pets + ' pet(s).')
+console.log('Fun fact: ' + funFact)
+
+// Reassigning the variables
+
+personName = "Khrong";
+pets = "17";
+funFact = "My khrong eats everything";
+
+
+console.log('My name is ' + personName + '.')
+console.log('I have ' + pets + ' pet(s).')
+console.log('Fun fact: ' + funFact)
