fbb94fb546a64c437c51999d6119291167e865ed
Kais Al Balkhi <qaiscyberlife@gmail.com>
2022-12-30 12:27:27 -0500

    solved 12-Stu_Arrays

--- in-class-workspace/03-Module/03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
+++ in-class-workspace/03-Module/03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
@@ -1,1 +1,14 @@
 // WRITE YOUR CODE HERE
+var studentNames = ['Qais', 'Rich', 'Will', 'Nancy'];
+
+console.log(studentNames.length)
+
+for (let i = 0; i < studentNames.length; i++) {
+    console.log('Welcome to the class ' + studentNames[i])
+}
+
+studentNames[0] = 'Qusay';
+
+if (studentNames[0] !== 'Qais') {
+ console.log(studentNames[0] + " is in class")
+}
