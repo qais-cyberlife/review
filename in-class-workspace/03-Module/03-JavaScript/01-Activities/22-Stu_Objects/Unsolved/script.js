b19e40ec129936cbe9b5d645b357bd9cf4495e11
Kais Al Balkhi <qaiscyberlife@gmail.com>
2022-12-30 18:12:45 -0500

    changed line 10 from customer.ready = true to customer.ready

--- in-class-workspace/03-Module/03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
+++ in-class-workspace/03-Module/03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
@@ -7,7 +7,7 @@
 
  console.log("The customer ordered a " + customerOrder.name + " with " + customerOrder.sugars + " sugars.")
 
- if (customerOrder.ready = true) {
+ if (customerOrder.ready) {
     console.log("Ready for pick-up")
  } else {
     console.log("Still in order queue")
