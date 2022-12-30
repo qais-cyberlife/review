dca0fcc10edc82ae9e3a8eb1a9a6984b3719c78f
Kais Al Balkhi <qaiscyberlife@gmail.com>
2022-12-30 18:09:44 -0500

    solved 22-Stu_Objects activity

--- in-class-workspace/03-Module/03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
+++ in-class-workspace/03-Module/03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
@@ -1,1 +1,14 @@
 //WRITE YOUR CODE BELOW
+ var customerOrder = {
+    name: "Black Tea",
+    sugars: 3, 
+    ready: true,
+ }
+
+ console.log("The customer ordered a " + customerOrder.name + " with " + customerOrder.sugars + " sugars.")
+
+ if (customerOrder.ready = true) {
+    console.log("Ready for pick-up")
+ } else {
+    console.log("Still in order queue")
+ }
