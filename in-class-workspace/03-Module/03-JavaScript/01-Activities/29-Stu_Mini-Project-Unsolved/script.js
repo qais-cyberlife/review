4b8760ce270f9e834ef0da76fca0d3419a3cc0cd
Kais Al Balkhi <qaiscyberlife@gmail.com>
2022-12-31 16:24:08 -0500

    added more features

--- in-class-workspace/03-Module/03-JavaScript/01-Activities/29-Stu_Mini-Project-Unsolved/script.js
+++ in-class-workspace/03-Module/03-JavaScript/01-Activities/29-Stu_Mini-Project-Unsolved/script.js
@@ -1,44 +1,52 @@
-var wins = 0; 
+var wins = 0;
 var loses = 0;
 var ties = 0;
 
+// Values for the computer to choose from
 values = ["Rock", "Scissors", "Paper"]
 
+var playGame = function () {
 
-var choice = prompt("Welcome to the Rock, Paper, Scissors Game! Choose R, S, or P.");
+    var choice = prompt("Welcome to the Rock, Paper, Scissors Game! Choose R, S, or P.");
 
 
-    var min = 0; 
+    var min = 0;
     var max = 3;
-    var randomInt = Math.floor(Math.random()*(max-min)+min);
+    var randomInt = Math.floor(Math.random() * (max - min) + min);
     var computer = values[randomInt];
 
+    if (!choice) {
+        return;
+    }
+    if (
+        choice === "R" && computer == "Rock" ||
+        choice === "S" && computer == "Scissors" ||
+        choice === "P" && computer == "Paper"
+    ) {
+        window.alert("It's a tie!");
+        ties++;
+    } else if (
+        choice === "R" && computer == "Scissors" ||
+        choice === "S" && computer == "Paper" ||
+        choice === "P" && computer == "Rock"
+    ) {
+        window.alert("You Won!");
+        wins++;
+    } else {
+        window.alert("You Lost!");
+        loses++;
+    }
 
-    function play() {
-        if ( 
-            choice === "R" && computer == "Rock" ||
-            choice === "S" && computer == "Scissors" ||
-            choice === "P" && computer == "Paper" 
-            ) {
-            window.alert("It's a tie!");
-            ties ++;
-        } else if (
-            choice === "R" && computer == "Scissors" ||
-            choice === "S" && computer == "Paper" ||
-            choice === "P" && computer == "Rock" 
-        ) {
-            window.alert("You Won!");
-            wins ++;
-        } else if (
-            choice === "R" && computer == "Paper" ||
-            choice === "S" && computer == "Rock" ||
-            choice === "P" && computer == "Scissors" 
-        ){
-            window.alert("You Lost!");
-            loses ++;
-        }
-        return
-        };
+    window.alert(
+        "Stats:\nWins: " + wins + "\nLosses: " + loses + "\nTies " + ties
+    );
+
+    var playAgain = window.confirm("Play Again?");
+
+    if (playAgain) {
+        playGame();
+    }
+};
+
 
 window.alert("The Computer choose " + computer);
-play ();
