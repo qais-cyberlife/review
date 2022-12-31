fbfb5d4aa4f27e114bbce8c4f7f8009234619fd0
Kais Al Balkhi <qaiscyberlife@gmail.com>
2022-12-31 17:56:17 -0500

    changed the questioning strategy to window.confirm instead of window.prompt

--- in-class-workspace/03-Module/03-JavaScript/02-Challenge/Develop/script.js
+++ in-class-workspace/03-Module/03-JavaScript/02-Challenge/Develop/script.js
@@ -2,29 +2,34 @@
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var capitalCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var symbols = ["!", "#", "$", "%" , "&" , "'" , "(" , ")" , "*" , "+" ,"," , "-" ,"." , "/" , ":" , ";" , "<" , "=" , ">" , "?" , "@" , "[" , "]"];
+var symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]"];
 var pass = [];
 
+
 var generateBtn = document.querySelector("#generate");
-choice = window.prompt("Enter the letter to the types of characters you want to use in your code. \nA) Numbers \nB) Capital Letters \nC) Lowercase Letter \nD) Symbols \n Ex. The Syntax is (abc) if you just want Numbers, Capital Letters, and Lowercase letters in your password")
+choice1 = window.confirm("Do you want your password to include Numbers?")
+choice2 = window.confirm("Do you want your password to include Capital Letters?")
+choice3 = window.confirm("Do you want your password to include Lowercase Letter?")
+choice4 = window.confirm("Do you want your password to include Symbols?")
+
 passLength = window.prompt("Enter the length of your password.")
 
-if (choice === "a") {
+if (choice1) {
   for (let i = 0; i < passLength; i++) {
     x = Math.floor(Math.random() * numbers.length);
     pass.push(numbers[x]);
   }
-} else if (choice === "b") {
+} else if (choice2) {
   for (let i = 0; i < passLength; i++) {
     x = Math.floor(Math.random() * capitalCase.length);
     pass.push(capitalCase[x]);
   }
-} else if (choice === "c") {
+} else if (choice3) {
   for (let i = 0; i < passLength; i++) {
     x = Math.floor(Math.random() * lowerCase.length);
     pass.push(lowerCase[x]);
   }
-} else if (choice === "d") {
+} else if (choice4) {
   for (let i = 0; i < passLength; i++) {
     x = Math.floor(Math.random() * symbols.length);
     pass.push(symbols[x]);
@@ -35,14 +40,54 @@ if (choice === "a") {
     x = Math.floor(Math.random() * combined.length);
     pass.push(combined[x]);
   }
+} else if (choice == "ac" || "ca") {
+  combined = numbers.concat(lowerCase)
+  for (let i = 0; i < passLength; i++) {
+    x = Math.floor(Math.random() * combined.length);
+    pass.push(combined[x]);
+  }
+} else if (choice == "ad" || "da") {
+  combined = numbers.concat(symbols)
+  for (let i = 0; i < passLength; i++) {
+    x = Math.floor(Math.random() * combined.length);
+    pass.push(combined[x]);
+  }
+} else if (choice == "bc" || "cb") {
+  combined = capitalCase.concat(lowerCase)
+  for (let i = 0; i < passLength; i++) {
+    x = Math.floor(Math.random() * combined.length);
+    pass.push(combined[x]);
+  }
+} else if (choice == "bd" || "db") {
+  combined = capitalCase.concat(symbols)
+  for (let i = 0; i < passLength; i++) {
+    x = Math.floor(Math.random() * combined.length);
+    pass.push(combined[x]);
+  }
+} else if (choice == "cd" || "dc") {
+  combined = capitalCase.concat(symbols)
+  for (let i = 0; i < passLength; i++) {
+    x = Math.floor(Math.random() * combined.length);
+    pass.push(combined[x]);
+  }
+} else 
+if (choice.length =3 && choice == "abc" || "acb" || "bac" || "bca" || "cab" || "cba") {
+  combined = numbers.concat(symbols, lowerCase);
+
+  for (let i = 0; i < passLength; i++) {
+    x = Math.floor(Math.random() * combined.length);
+    pass.push(combined[x]);
+  }
 }
 
+console.log(combined);
 
-console.log(pass)
+
+// console.log(pass)
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();abc
+  var password = generatePassword(); abc
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
