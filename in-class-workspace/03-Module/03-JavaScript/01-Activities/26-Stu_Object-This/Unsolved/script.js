35eb557b94245f8f4b94d734c8777591b8c57588
Kais Al Balkhi <qaiscyberlife@gmail.com>
2022-12-30 19:05:25 -0500

    solved 26-Stu_Object

--- in-class-workspace/03-Module/03-JavaScript/01-Activities/26-Stu_Object-This/Unsolved/script.js
+++ in-class-workspace/03-Module/03-JavaScript/01-Activities/26-Stu_Object-This/Unsolved/script.js
@@ -1,12 +1,15 @@
 // TODO: What does 'this' refer to?
+// The global object in this case, the Window Object
 console.log(this);
 
 // TODO: What does 'this' refer to?
+// Window Object
 function helloThis() { 
    console.log("Inside this function, this is " + this); 
    }
 
 // TODO: What will this log? 
+// this refers to the object, child. this.age is equivilent to child.age. It will log 20.
 var child = { 
    age: 10,
    ageTenYears: function() {
@@ -15,6 +18,7 @@ var child = {
 };
 
 // TODO: What will this log? 
+// 5750
 var investor = {  
   name: "Cash Saver",
   investment: {
