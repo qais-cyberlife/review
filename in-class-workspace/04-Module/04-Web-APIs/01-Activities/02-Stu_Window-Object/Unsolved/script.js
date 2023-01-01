219e2d45efca659c95c61f26979393f7e2b5915d
Kais Al Balkhi <qaiscyberlife@gmail.com>
2023-01-01 16:00:07 -0500

    Sovled 02-Stu_WIndow-Object

--- in-class-workspace/04-Module/04-Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js
+++ in-class-workspace/04-Module/04-Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js
@@ -1,12 +1,16 @@
 // TODO: What will the following line of code log?
+// Logs the Window Object
 console.log(window);
 
 // TODO: What will the following line of code log?
+// Logs the Document Object which is a Child of the WIndow Object
 console.log(window.document);
 
 // TODO: What will the following line of code log?
+// Logs the HTML element because it is the root element of the documents object
 console.log(document.documentElement);
 
 // TODO: What will the following line of code log?
+// Logs the Head which is the child of the document object
 console.log(document.head);
 
