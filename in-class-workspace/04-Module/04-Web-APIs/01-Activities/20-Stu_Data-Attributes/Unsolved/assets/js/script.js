2027f82a6d3782c3a3d2d4311d752dc962fcad67
Qais-cyberlife <qaiscyberlife@gmail.com>
2023-01-04 14:53:33 -0500

    solved activity

--- in-class-workspace/04-Module/04-Web-APIs/01-Activities/20-Stu_Data-Attributes/Unsolved/assets/js/script.js
+++ in-class-workspace/04-Module/04-Web-APIs/01-Activities/20-Stu_Data-Attributes/Unsolved/assets/js/script.js
@@ -1,7 +1,18 @@
 var container = document.querySelector(".container");
 
-container.addEventListener("click", function(event) {
+container.addEventListener("click", function (event) {
   var element = event.target;
+  var number = element.getAttribute("data-number")
 
   // TODO: Complete function
+  if (element.matches("div")) {
+    var state = element.getAttribute("data-state");
+    if (state === "hidden"){
+      element.setAttribute("data-state", "block");
+      element.textContent = number;
+    } else if (state === "block") {
+      element.setAttribute("data-state", "hidden");
+        element.textContent = "";
+      }
+  }
 });
