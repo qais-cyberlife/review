da09e7be1687ded59b8094aa264435b58ff7be16
Qais-cyberlife <qaiscyberlife@gmail.com>
2023-01-08 12:22:59 -0500

    solved activity #2

--- in-class-workspace/05-Module/05-Third-Party-APIs/01-Activities/01-Ins_jQuery-Elements/assets/js/script.js
+++ in-class-workspace/05-Module/05-Third-Party-APIs/01-Activities/01-Ins_jQuery-Elements/assets/js/script.js
@@ -17,7 +17,7 @@ titleEl.addClass('p-5');
 
 // titleEl.style.border = "rgb(122, 242, 242) 3px solid";
 titleEl.css('border', 'rgb(122, 242, 242) 5px solid');
-
+ 
 // rootEl.appendChild(titleEl);
 rootEl.append(titleEl);
 
