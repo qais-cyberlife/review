5af5f8df4c2292d9513379bc9de3725c3f3fc445
Qais-cyberlife <qaiscyberlife@gmail.com>
2023-01-08 13:36:36 -0500

    solved activity #6

--- in-class-workspace/05-Module/05-Third-Party-APIs/01-Activities/06-Stu_Form-Elements/Unsolved/assets/js/script.js
+++ in-class-workspace/05-Module/05-Third-Party-APIs/01-Activities/06-Stu_Form-Elements/Unsolved/assets/js/script.js
@@ -2,17 +2,26 @@ var shoppingFormEl = $('#shopping-form');
 var shoppingListEl = $('#shopping-list');
 
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
-var items = [];
 
-$.each(shoppingListEl, function () {
-var itemEl = $('input[name="shopping-input"]');
-li = $('<li>');
-li.append(itemsEl)
+function addItem(event) {
+event.preventDefault();
+
+// selevt form element by its 'name; attr. and get its value
+
+var shoppingItem = $('input[name="shopping-input').val();
+
+// if there's nothing in the form entered, don't print to the page
+if (!shoppingItem){
+console.log('No shopping item filled out in form!'); 
+return;
 }
-)
 
+// Print to the page
+shoppingListEl.append('<li>' + shoppingItem + '</li>');
 
-
+// Clear the form input element
+$('input[name="shopping-input"]').val('');
+}
 
 // TODO: Add an event listener to the `shoppingFormEl` to handle submission
-shoppingFormEl.on('submit', addShoppingItem);
+shoppingFormEl.on('submit', addItem);
