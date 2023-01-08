452341ab29444220db3109b6f4d0b9e3995c1108
Qais-cyberlife <qaiscyberlife@gmail.com>
2023-01-08 14:53:53 -0500

    solved activity #8

--- in-class-workspace/05-Module/05-Third-Party-APIs/01-Activities/08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
+++ in-class-workspace/05-Module/05-Third-Party-APIs/01-Activities/08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
@@ -1,6 +1,12 @@
 // Assemble: Create/select DOM elements
 var rootEl = $('#root');
+var rowEl = $(rootEl).children('ul');
+var boxEl = $(rowEl).children('li');
 
 // TODO: Starting from the root element, `rootEl`, make all the background color for each box white.
+rootEl.children().children().css('background-color', 'white');
 
 // TODO: Starting from `rootEl`, create the jQuery statement that will add "O" to block the "X" from winning
+
+rootEl.children('ul').children('li').eq(6).text("O");
+rootEl.children('ul').children('li').eq(0).text("O");
