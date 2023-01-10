0a5e531d2e22a029c8e9d16c4253426f00580dfe
Qais-cyberlife <qaiscyberlife@gmail.com>
2023-01-09 19:00:47 -0500

    solved activity #25

--- in-class-workspace/05-Module/05-Third-Party-APIs/01-Activities/26-Stu_Moment-Format/Unsolved/assets/js/script.js
+++ in-class-workspace/05-Module/05-Third-Party-APIs/01-Activities/26-Stu_Moment-Format/Unsolved/assets/js/script.js
@@ -1,25 +1,27 @@
 // Use Moment.js to format the date and assign to the declared variable.
-// TODO: 1. What is your graduation date in the following format: Jan 1st, 1999?
-var gradDate;
-$("#1a").text(gradDate);
+// TODO: 1. What is your graduation date in the following format: Jan 1st, 1999? 
+var gradDate = moment("05/02/2019", "MM-DD-YYYY"); ;
+$("#1a").text(gradDate.format("MMM Do, YYYY"));
 
 // TODO: 2. What day of the week will 1/1/2022 be?
-var weekDay;
-$("#2a").text(weekDay);
+var weekDay = moment("1/1/2022", "MM-DD-YYYY");
+$("#2a").text(weekDay.format("dddd"));
 
 // TODO: 3. Out of 365, what day of the year is today?
-var now;
-$("#3a").text(now);
+var now = moment();
+$("#3a").text(now.format("DDD"));
 
 // TODO: 4. What is the current time in the format: hours:minutes:seconds
-var time;
-$("#4a").text(time);
+var time = moment();
+$("#4a").text(time.format("HH:mm:ss"));
 
 // TODO: 5. What is the current Unix timestamp?
-var unix;
+var unix = moment().unix();
 $("#5a").text(unix);
 
 // TODO: 6. Parse the following Unix timestamp, 1318781876, and convert into any time/date format.
-var unixFormat;
-$("#6a").text(unixFormat);
+var unixFormat = moment();
+const timestamp = 1318781876;
+const date = moment.unix(timestamp);
+$("#6a").text(date.format('dddd, MMMM Do YYYY'));
 
