fd60dae1281da8972d48825d2aff9bc0bba5ac06
Qais-cyberlife <qaiscyberlife@gmail.com>
2023-01-10 11:14:49 -0500

    copied the min-project activities folder and removed the solutions

---
+++ in-class-workspace/05-Module/05-Third-Party-APIs/01-Activities/29-Stu_Mini-Project copy/Main/assets/js/script.js
@@ -0,0 +1,12 @@
+// save reference to important DOM elements
+
+
+// handle displaying the time
+
+// handle printing project data to the page
+
+// You can also chain methods onto new lines to keep code clean
+
+// By listing each `<td>` variable as an argument, each one will be appended in that order
+
+// handle project form submission
