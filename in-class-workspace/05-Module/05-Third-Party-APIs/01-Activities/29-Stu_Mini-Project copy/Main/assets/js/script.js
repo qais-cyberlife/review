9d55d2c3e846ac245ee8f0d36db0dbec8c814b17
Qais-cyberlife <qaiscyberlife@gmail.com>
2023-01-11 15:09:00 -0500

    modal can be called now, date picker added via Javascript

--- in-class-workspace/05-Module/05-Third-Party-APIs/01-Activities/29-Stu_Mini-Project copy/Main/assets/js/script.js
+++ in-class-workspace/05-Module/05-Third-Party-APIs/01-Activities/29-Stu_Mini-Project copy/Main/assets/js/script.js
@@ -1,4 +1,13 @@
 // save reference to important DOM elements
+// save reference to important DOM elements
+var timeDisplayEl = $('#time-display');
+var projectDisplayEl = $('#project-display');
+var projectModalEl = $('#project-modal');
+var projectFormEl = $('#project-form');
+var projectNameInputEl = $('#project-name-input');
+var projectTypeInputEl = $('#project-type-input');
+var hourlyRateInputEl = $('#hourly-rate-input');
+var dueDateInputEl = $('#due-date-input');
 
 
 // handle displaying the time
@@ -10,4 +19,5 @@
 // By listing each `<td>` variable as an argument, each one will be appended in that order
 
 // handle project form submission
-
+dueDateInputEl.datepicker({ minDate: 1 });
+$('#project-modal').modal(options)
