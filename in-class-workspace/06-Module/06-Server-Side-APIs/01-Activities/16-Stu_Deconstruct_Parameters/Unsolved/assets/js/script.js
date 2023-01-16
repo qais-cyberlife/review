723a6d5723c6d10e4e0abaea3b833a9110dd16b3
Qais-cyberlife <qaiscyberlife@gmail.com>
2023-01-16 14:42:14 -0500

    solved activity #16

--- in-class-workspace/06-Module/06-Server-Side-APIs/01-Activities/16-Stu_Deconstruct_Parameters/Unsolved/assets/js/script.js
+++ in-class-workspace/06-Module/06-Server-Side-APIs/01-Activities/16-Stu_Deconstruct_Parameters/Unsolved/assets/js/script.js
@@ -9,3 +9,10 @@ fetch(
     console.log(data);
   });
 // Parameter explanation.
+// per_page=10: This parameter specifies the number of results to be returned per page. In this case, it is set to 10.
+
+// state=open: This parameter filters the issues to only those that are currently open.
+
+// sort=created: This parameter specifies the sorting of the returned issues based on their creation date.
+
+// direction=desc: This parameter specifies the sorting direction, which is set to descending in this case.
