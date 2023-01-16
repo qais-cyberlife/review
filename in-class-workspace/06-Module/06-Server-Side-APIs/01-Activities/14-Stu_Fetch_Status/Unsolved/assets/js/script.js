52e121b8e75e8c1156344e766d3c94f15399d48a
Qais-cyberlife <qaiscyberlife@gmail.com>
2023-01-16 13:58:18 -0500

    added changes

--- in-class-workspace/06-Module/06-Server-Side-APIs/01-Activities/14-Stu_Fetch_Status/Unsolved/assets/js/script.js
+++ in-class-workspace/06-Module/06-Server-Side-APIs/01-Activities/14-Stu_Fetch_Status/Unsolved/assets/js/script.js
@@ -2,8 +2,8 @@ var badRequestUrl = 'https://api.github.com/orgs/nodejs/oreps?per_page=5';
 
 var responseText = document.getElementById('response-text');
 
-function getApi(badRequestUrl) {
-  fetch(badRequestUrl)
+function getApi(requestUrl) {
+  fetch(requestUrl)
     .then(function (response) {
       // Check the console first to see the response.status
       console.log(response.status);
