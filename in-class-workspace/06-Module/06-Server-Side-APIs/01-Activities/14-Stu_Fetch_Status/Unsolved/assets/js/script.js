dcdbf117317411657580f3e94679ea474dffb363
Qais-cyberlife <qaiscyberlife@gmail.com>
2023-01-16 13:56:20 -0500

    solved activity #14

--- in-class-workspace/06-Module/06-Server-Side-APIs/01-Activities/14-Stu_Fetch_Status/Unsolved/assets/js/script.js
+++ in-class-workspace/06-Module/06-Server-Side-APIs/01-Activities/14-Stu_Fetch_Status/Unsolved/assets/js/script.js
@@ -2,13 +2,16 @@ var badRequestUrl = 'https://api.github.com/orgs/nodejs/oreps?per_page=5';
 
 var responseText = document.getElementById('response-text');
 
-function getApi(request) {
-  fetch(requestUrl)
+function getApi(badRequestUrl) {
+  fetch(badRequestUrl)
     .then(function (response) {
       // Check the console first to see the response.status
       console.log(response.status);
       // Then write the conditional based on that response.status value
       // Make sure to display the response on the page
+      if (response.status !== 200) {
+        responseText.textContent = response.status
+      }
     })
     .then(function (data) {
       console.log(data);
