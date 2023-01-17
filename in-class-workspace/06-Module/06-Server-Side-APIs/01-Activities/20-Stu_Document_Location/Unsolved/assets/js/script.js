99a23df43ed6ca6547424ce72eeedc1dd6647cef
Qais-cyberlife <qaiscyberlife@gmail.com>
2023-01-17 11:49:20 -0500

    solved activity #20

--- in-class-workspace/06-Module/06-Server-Side-APIs/01-Activities/20-Stu_Document_Location/Unsolved/assets/js/script.js
+++ in-class-workspace/06-Module/06-Server-Side-APIs/01-Activities/20-Stu_Document_Location/Unsolved/assets/js/script.js
@@ -4,8 +4,4 @@ var redirectUrl = './404.html';
 fetch(badRequestUrl).then(function (response) {
   // Use a conditional to check the response status.
   // If that status equals the conditional, then redirect to the 404 page.
- console.log(response)
- if (response.status == 404) {
-  document.location.replace(redirectUrl)
- }
 });
