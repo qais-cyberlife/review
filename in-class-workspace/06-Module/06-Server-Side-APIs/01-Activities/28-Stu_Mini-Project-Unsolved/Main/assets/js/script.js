21103d6961d10da0416d62a23431a063833f8667
Qais-cyberlife <qaiscyberlife@gmail.com>
2023-01-17 15:49:39 -0500

    the request url is working, now I have to returna  json response and save specific parameters as variable for them to be rendered into the display search [age

--- in-class-workspace/06-Module/06-Server-Side-APIs/01-Activities/28-Stu_Mini-Project-Unsolved/Main/assets/js/script.js
+++ in-class-workspace/06-Module/06-Server-Side-APIs/01-Activities/28-Stu_Mini-Project-Unsolved/Main/assets/js/script.js
@@ -3,30 +3,42 @@ var searchFormEl = $('#search-form')
 var searchInputEl = $('#search-input');
 var formatInputEl = $('#format-input');
 
-// console.log(formEl);
-// console.log(searchInputEl);
-// console.log(formatInputEl);
+// console.log(formEl); Successful
+// console.log(searchInputEl); Successful
+// console.log(formatInputEl); Successful
 
 function handleFormSubmit(event) {
   event.preventDefault();
 
-  var search = searchInputEl.val();
-  // console.log(search)
+  var search = searchInputEl.val().trim();
+  // console.log(search) Successful
 
   var format = formatInputEl.val();
-  // console.log(format)
+  // console.log(format) Successful
 
-  if (format) {
-    redirectUrl = "./search-results.html?q=" + search + "&format=" + format;
-  document.location.replace(redirectUrl);
-  } else if (!search) {
-  window.alert("Please enter a search query! :)")
+  if (!search) {
+    window.alert("Please enter a search query! :)")
 
+  } else if (format) {
+  redirectUrl = "./search-results.html?q=" + search + "&format=" + format;
+  // document.location.replace(redirectUrl);
+  // console.log(format) Successful
+  getData(search, format)
 }else {
   redirectUrl = "./search-results.html?q=" + search + "&format=";
-  document.location.replace(redirectUrl);
+  // document.location.replace(redirectUrl);
+  getData(search, "")
+  // console.log(format) Successful
 }
 }
 
+var getData = function (search, format){
+  if (format) {
+    var apiUrl = 'https://www.loc.gov/' + format + '/?q=' + search + '&fo=json';
+  } else {
+  var apiUrl = 'https://www.loc.gov/search/?q=' + search + '&fo=json';
+}
+// console.log(apiUrl) Successful
+}
 
 searchFormEl.on('submit', handleFormSubmit);
